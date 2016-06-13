692b97053f7722f736a57c2149967f3c2a7c92a2
miguel <miguelpayet@gmail.com>
2016-06-12 21:19:55 -0500

    añadir la clase imagen a la celda

--- src/main/pe/trazos/homepage/ImagePopulatorCompetencia.java
+++ src/main/pe/trazos/homepage/ImagePopulatorCompetencia.java
@@ -1,5 +1,6 @@
 package pe.trazos.homepage;
 
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
 import org.apache.wicket.extensions.markup.html.repeater.data.grid.PropertyPopulator;
 import org.apache.wicket.markup.repeater.Item;
@@ -20,6 +21,7 @@ class ImagePopulatorCompetencia<T> extends PropertyPopulator<T> {
 	public void populateItem(final Item<ICellPopulator<T>> cellItem, final String componentId, final IModel<T> rowModel) {
 		ImagePanel celda = new ImagePanel(componentId, new PropertyModel<>(rowModel, propiedad));
 		cellItem.add(celda);
+		cellItem.add(new AttributeModifier("class", "imagen"));
 	}
 
 }
