f5017dfa90bd048086789bf85540e1815b8efc79
miguel <miguelpayet@gmail.com>
2016-06-18 22:58:31 -0500

    añadir un método para obtener los pronósticos válidos de una fecha.

--- src/main/pe/trazos/dao/DaoPronostico.java
+++ src/main/pe/trazos/dao/DaoPronostico.java
@@ -1,14 +1,20 @@
 package pe.trazos.dao;
 
 import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.criterion.Restrictions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pe.trazos.dominio.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class DaoPronostico extends Dao<Pronostico> {
+
+	private final static Logger log = LoggerFactory.getLogger(DaoPronostico.class);
 
 	public DaoPronostico() {
 		super(Pronostico.class);
@@ -27,11 +33,18 @@ public class DaoPronostico extends Dao<Pronostico> {
 
 	public List<Pronostico> get(Visitante unVisitante) {
 		Criteria criteria = getSession().createCriteria(getClaseEntidad());
-		criteria.add(Restrictions.eq("visitante", unVisitante.getUserId()));
+		criteria.add(Restrictions.eq("visitante", unVisitante));
 		List<Pronostico> pronosticos = criteria.list();
 		return pronosticos;
 	}
 
+	/**
+	 * obtener una lista de pronósticos para una fecha
+	 *
+	 * @param unVisitante -> visitante cuyos pronósticos se desea obtener
+	 * @param unaFecha -> fecha cuyos pronosticos se obtiene
+	 * @return mapa indexado por participación con los pronósticos obtenidos
+	 */
 	public Map<Participacion, Pronostico> obtenerPronosticosFecha(Visitante unVisitante, Fecha unaFecha) {
 		Map<Participacion, Pronostico> pronosticos = new HashMap<>();
 		// obtener lista de partidos de la fecha
@@ -46,4 +59,38 @@ public class DaoPronostico extends Dao<Pronostico> {
 		// devolver la lista
 		return pronosticos;
 	}
+
+	/**
+	 * obtener una lista de pronósticos válidos (goles no nulos) para una fecha
+	 *
+	 * @param unaFecha -> fecha cuyos pronosticos se obtiene
+	 * @return mapa indexado por participación con los pronósticos obtenidos
+	 */
+	public Map<Participacion, Pronostico> obtenerValidos(Fecha unaFecha) {
+		//Criteria criteria = getSession().createCriteria(getClaseEntidad());
+		StringBuilder sb = new StringBuilder();
+		sb.append("from Pronostico p ").append("where p.visitante = '").append(unaFecha.getCompetencia().getVisitante().getId()).append("'");
+		sb.append("and p.goles is not null ");
+		sb.append("and p.participacion in (");
+		ArrayList<Participacion> participantes = new ArrayList<>();
+		for (Partido partido : unaFecha.getPartidos()) {
+			participantes.addAll(partido.getParticipantes().values());
+		}
+		String[] lista = new String[participantes.size()];
+		int i = 0;
+		for (Participacion part : participantes) {
+			lista[i++] = "'" + part.getId() + "'";
+		}
+		sb.append(String.join(",", lista));
+		sb.append(")");
+		log.debug(sb.toString());
+		Query query = session.createQuery(sb.toString());
+		List<Pronostico> list = query.list();
+		Map<Participacion, Pronostico> pronosticos = new HashMap<>();
+		for (Pronostico pron : list) {
+			pronosticos.put(pron.getParticipacion(), pron);
+		}
+		return pronosticos;
+	}
+
 }
