db8d6b33f19afc27caae74ec16863f9b9ba71ab5
miguel <miguelpayet@gmail.com>
2016-06-18 22:59:01 -0500

    agregar una prueba de las validaciones a nivel de fecha que afectan pronósticos

--- src/test/pe/trazos/main/TestPronostico.java
+++ src/test/pe/trazos/main/TestPronostico.java
@@ -7,24 +7,20 @@ import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import pe.trazos.dao.DaoFecha;
-import pe.trazos.dao.DaoPronostico;
-import pe.trazos.dao.DaoVisitante;
-import pe.trazos.dao.HibernateUtil;
+import pe.trazos.dao.*;
 import pe.trazos.dominio.*;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import static junit.framework.TestCase.assertNotNull;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 public class TestPronostico extends TestBase {
 
-	private final Logger logger = LoggerFactory.getLogger(TestPronostico.class);
+	private final static int ID_COMPETENCIA = 1;
+	private final static int ID_FECHA = 307;
+	private final static String USER_ID = "58213393-6c5b-4252-99ea-d4dbcec282f4";
+	private final static Logger log = LoggerFactory.getLogger(TestPronostico.class);
 
 	@After
 	public void endTransaction() {
@@ -34,31 +30,24 @@ public class TestPronostico extends TestBase {
 	@Test
 	public void leerPronosticosFecha() {
 		DaoVisitante dv = new DaoVisitante();
-		Visitante v = dv.get("10208978070830655");
+		Visitante v = dv.get(USER_ID);
 		assertNotNull(v);
 		DaoFecha df = new DaoFecha();
-		Fecha f = df.get(307);
+		Fecha f = df.get(ID_FECHA);
 		assertNotNull(f);
 		DaoPronostico dp = new DaoPronostico();
 		Map<Participacion, Pronostico> pronosticos = dp.obtenerPronosticosFecha(v, f);
 		for (Pronostico p : pronosticos.values()) {
-			logger.info(String.format("%s - %s", p.getParticipacion().getEquipo().getNombre(), p.getGoles()));
+			log.info("{} - {}", p.getParticipacion().getEquipo().getNombre(), p.getGoles());
 			Assert.assertNotNull(p.getGoles());
 		}
 	}
 
 	@Test
-	public void leerPronosticosId() {
-		DaoPronostico dp = new DaoPronostico();
-		Pronostico pronostico = dp.get(1);
-		Assert.assertNotNull(pronostico);
-		logger.info(String.format("%s - %s", pronostico.getVisitante().getNombre(), pronostico.getVisitante().getUserId()));
-	}
-
-	@Test
 	public void leerPronosticosVisitante() {
 		DaoVisitante dv = new DaoVisitante();
-		Visitante v = dv.get("10208978070830655");
+		Visitante v = dv.get(USER_ID);
+		Assert.assertNotNull(v);
 		DaoPronostico dp = new DaoPronostico();
 		List<Pronostico> pronosticos = dp.get(v);
 		Assert.assertNotEquals(pronosticos.size(), 0);
@@ -69,6 +58,22 @@ public class TestPronostico extends TestBase {
 		SessionFactory sf = HibernateUtil.getSessionFactory();
 		sesion = sf.getCurrentSession();
 		sesion.beginTransaction();
+	}
+
+	@Test
+	public void validarFechasPronosticadas() {
+		DaoVisitante dv = new DaoVisitante();
+		Visitante v = dv.get(USER_ID);		DaoCompetencia dc = new DaoCompetencia();
+		Competencia c = dc.get(ID_COMPETENCIA);
+		c.setVisitante(v);
+		for (Fecha f : c.getFechas()) {
+			log.info("{}", f.toString());
+			// si está pronosticada completamente por el visitante devuelve rtue, si no false
+			log.info("tiene pronóstico {} ", f.tienePronostico());
+			// si ya pasó la fecha y no aplica pronostico
+			f.aplicaPronostico();
+			log.info("aplica pronosticar {} ", f.aplicaPronostico());
+		}
 	}
 
 }
