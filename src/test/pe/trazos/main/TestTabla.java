e5297a9c8a990b2a3d29d37f8f76f2c173617e1c
miguel <miguelpayet@gmail.com>
2016-06-18 22:59:43 -0500

    burrar una prueba incompleta / esta clase queda vacía por el momento

--- src/test/pe/trazos/main/TestTabla.java
+++ src/test/pe/trazos/main/TestTabla.java
@@ -11,15 +11,4 @@ import pe.trazos.dominio.Visitante;
 
 public class TestTabla extends TestBase {
 
-	@Test
-	public void testTabla() {
-		Session sesion = createSession();
-		Transaction tx = sesion.beginTransaction();
-		DaoVisitante dv = new DaoVisitante();
-		Visitante v = dv.get("10208978070830655");
-		boolean l = SesionWeb.get().loginFacebook(v.getUserId(), v.getToken());
-		Assert.assertThat(l, org.hamcrest.CoreMatchers.is(true));
-		tx.rollback();
-	}
-
 }
