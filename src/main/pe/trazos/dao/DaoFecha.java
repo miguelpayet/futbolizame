9cb4da68638273dc175b970360cce32c853532e9
miguel <miguelpayet@gmail.com>
2016-06-20 13:52:38 -0500

    metodo para listar anteriores y futuras

--- src/main/pe/trazos/dao/DaoFecha.java
+++ src/main/pe/trazos/dao/DaoFecha.java
@@ -2,10 +2,13 @@ package pe.trazos.dao;
 
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Restrictions;
+import pe.trazos.dominio.Competencia;
 import pe.trazos.dominio.Fecha;
 
 import java.util.Date;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class DaoFecha extends Dao<Fecha> {
 
@@ -13,10 +16,19 @@ public class DaoFecha extends Dao<Fecha> {
 		super(Fecha.class);
 	}
 
-	public List<Fecha> getFuturo(Date unaFecha) {
+	public Set<Fecha> listarAnteriores(Fecha unaFecha) {
 		Criteria criteria = getSession().createCriteria(getClaseEntidad());
-		criteria.add(Restrictions.lt("fecha", unaFecha));
-		return criteria.list();
+		criteria.add(Restrictions.eq("competencia", unaFecha.getCompetencia()));
+		criteria.add(Restrictions.lt("fecha", unaFecha.getFecha()));
+		return new LinkedHashSet<>((List<Fecha>) criteria.list());
 	}
+
+	public Set<Fecha> listarFuturas(Competencia unaCompetencia, Date unaFecha) {
+		Criteria criteria = getSession().createCriteria(getClaseEntidad());
+		criteria.add(Restrictions.eq("competencia", unaCompetencia));
+		criteria.add(Restrictions.lt("fecha", unaFecha));
+		return new LinkedHashSet<>((List<Fecha>) criteria.list());
+	}
+
 
 }
