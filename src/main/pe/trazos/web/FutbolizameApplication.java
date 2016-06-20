5135b900cbd7877a308cde16808cdab1a6365dd2
miguel <miguelpayet@gmail.com>
2016-06-20 13:53:05 -0500

    quitar el logger y reactivar los ids de wicket

--- src/main/pe/trazos/web/FutbolizameApplication.java
+++ src/main/pe/trazos/web/FutbolizameApplication.java
@@ -8,9 +8,12 @@ import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.request.Request;
 import org.apache.wicket.request.Response;
+import org.apache.wicket.request.Url;
+import org.apache.wicket.request.cycle.RequestCycle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pe.trazos.auth.SesionWeb;
+import pe.trazos.componentes.WebPageBase;
 import pe.trazos.homepage.HomePage;
 
 public class FutbolizameApplication extends WebApplication {
@@ -18,8 +21,6 @@ public class FutbolizameApplication extends WebApplication {
 	public static FutbolizameApplication get() {
 		return (FutbolizameApplication) Application.get();
 	}
-
-	private final Logger logger = LoggerFactory.getLogger(FutbolizameApplication.class);
 
 	public FutbolizameApplication() {
 	}
@@ -34,11 +35,9 @@ public class FutbolizameApplication extends WebApplication {
 	public void init() {
 		super.init();
 		Bootstrap.install(this);
-/*
 		if (getConfigurationType() == RuntimeConfigurationType.DEVELOPMENT) {
 			getDebugSettings().setOutputComponentPath(true);
 		}
-		*/
 		getRequestCycleListeners().add(new HibernateRequestListener());
 	}
 
