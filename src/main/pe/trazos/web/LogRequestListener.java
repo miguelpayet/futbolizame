1fe53a4aedc9b873ae72041b99d216947f60abb4
miguel <miguelpayet@gmail.com>
2016-11-15 19:53:22 -0500

    listener para log y futuro análisis

---
+++ src/main/pe/trazos/web/LogRequestListener.java
@@ -0,0 +1,61 @@
+package pe.trazos.web;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.wicket.request.IRequestHandler;
+import org.apache.wicket.request.Request;
+import org.apache.wicket.request.Url;
+import org.apache.wicket.request.cycle.IRequestCycleListener;
+import org.apache.wicket.request.cycle.RequestCycle;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class LogRequestListener implements IRequestCycleListener {
+
+	public static Logger log = LogManager.getLogger(LogRequestListener.class);
+
+	public LogRequestListener() {
+	}
+
+	@Override
+	public void onBeginRequest(RequestCycle cycle) {
+		Request req = cycle.getRequest();
+		HttpServletRequest servletReq = (HttpServletRequest) req.getContainerRequest();
+		log.debug("onBeginRequest - {} ({}) - [{}]", servletReq.getRemoteHost(), servletReq.getRemoteAddr(), req.getOriginalUrl());
+	}
+
+	@Override
+	public void onDetach(RequestCycle cycle) {
+	}
+
+	@Override
+	public void onEndRequest(RequestCycle cycle) {
+		log.debug("onEndRequest {}", cycle.getRequest().getOriginalUrl());
+	}
+
+	@Override
+	public IRequestHandler onException(RequestCycle cycle, Exception ex) {
+		return null;
+	}
+
+	@Override
+	public void onExceptionRequestHandlerResolved(RequestCycle cycle, IRequestHandler handler, Exception exception) {
+	}
+
+	@Override
+	public void onRequestHandlerExecuted(RequestCycle cycle, IRequestHandler handler) {
+	}
+
+	@Override
+	public void onRequestHandlerResolved(RequestCycle cycle, IRequestHandler handler) {
+	}
+
+	@Override
+	public void onRequestHandlerScheduled(RequestCycle cycle, IRequestHandler handler) {
+	}
+
+	@Override
+	public void onUrlMapped(RequestCycle cycle, IRequestHandler handler, Url url) {
+	}
+
+}
